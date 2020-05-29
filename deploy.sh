3c9135b0775ab212549d6f69a7ade0cbf6036210
Gobin Sougrakpam <gobinsougrakpam@gmail.com>
2020-05-29 15:38:08 +1000

    Init

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-make docker-build IMG=localhost:5000/vault-controller:latest
-make docker-push IMG=localhost:5000/vault-controller:latest
-make undeploy IMG=localhost:5000/vault-controller:latest
-make deploy IMG=localhost:5000/vault-controller:latest
